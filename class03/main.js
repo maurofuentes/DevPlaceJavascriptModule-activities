955dd9f5d8a116ed7040267c7fb0e3a6364191bf
maurofuentes <maurofuentes983@gmail.com>
2022-08-27 02:42:04 -0300

    class03 - array methods

---
+++ class03/main.js
@@ -0,0 +1,53 @@
+const personalData = [];
+
+let dataAmount = 0;
+
+let maxDataAmount = 10;
+
+
+
+function saveData(){
+
+    
+
+    const names = document.querySelector('#namesValue').value;
+    const lastnames = document.querySelector('#lastnamesValue').value;
+    const telephone = document.querySelector('#telephoneValue').value;
+    const personalDataDiv = document.querySelector('#personalDataContainer');
+    const ulPersonalData = document.createElement('ul');
+
+    const userPersonalData = {
+        name : names,
+        lastname : lastnames,
+        tel : telephone
+    }
+
+    personalData.push(userPersonalData);
+
+    console.log(personalData);
+
+    dataAmount++;
+
+    // document.querySelector('#namesValue').value = '';
+    // document.querySelector('#lastnamesValue').value = '';
+    // document.querySelector('#telephoneValue').value = '';
+
+    if(dataAmount == maxDataAmount){
+
+        document.querySelector('#saveBtn').setAttribute('disabled', 'disabled');
+
+        for(let i = 0; i < personalData.length; i++){
+            
+            var row = document.createElement("li");
+
+            row.textContent = `${personalData[i].name} ${personalData[i].lastname} ${personalData[i].tel}`;
+            
+            ulPersonalData.appendChild(row);
+
+            personalDataContainer.appendChild(ulPersonalData);
+        }
+        
+    }
+    
+
+}
